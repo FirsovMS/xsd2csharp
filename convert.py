dedb0515b622442ec0fdb61ed4b531a5473a6b73
FirsovMS <firsovms1993@gmail.com>
2018-08-07 17:05:37 +0300

    добавлена шняга для борьбы с дупликатами, необходимо учитывать пересечения внутри всех классов

--- convert.py
+++ convert.py
@@ -3,6 +3,143 @@
 import os
 import subprocess
 import shutil
+import re
+
+# Simple csharp clsses parser
+class Parser:    
+    def __init__(self):
+        self.regex_class_begin = r"class\s+([a-zA-Z]+)\s+{";
+        self.regex_atribute = r"\[[\w|\.|(|,|\"|)|=|\s]+]";
+
+    # Позволяет парсить классы из cs файлов
+    # для общего файла фозвращяет map<file, hash>
+    # для других [file, (start_pos, end_pos)]
+    def parse_file(self, content, is_common_file):
+        print("Log: parse {} file".format("common" if is_common_file else "class"));
+        pattern_class_begin = re.compile(self.regex_class_begin);
+        map_file = dict() if is_common_file else list();
+        for i in range(len(content)):
+            match = pattern_class_begin.search(content[i])
+            if match != None:
+                start = i;
+                #find class end
+                open_braces = 1;
+                while(open_braces != 0):
+                    i += 1;
+                    st = content[i];
+                    if st.find('{') != -1:
+                        open_braces += 1;
+                    if st.find('}') != -1:
+                        open_braces -= 1;
+                end = i;
+                # find attributes
+##                back_ind = start - 1;
+##                while( True ):
+##                    if is_atttribute( content[back_ind] ):
+##                        back_ind -= 1;
+##                        continue;
+##                    break;
+##                start = start if back_ind == start - 1 else back_ind + 1;
+                # add to map
+                if is_common_file:
+                    #print ("Class was found at {0}-{1}: name: {class_name}".format( start, end, class_name = match.groups()[0]))
+                    map_file[match.groups()[0]] = hash(str(content[start : end]));
+                else:
+                    map_file.append( ( (match.groups()[0], hash(str(content[start : end]))) , (start, end)) );
+        print("Found: {} classes".format(len(map_file)));
+        return map_file;
+
+    # return back start, end position
+    def find_position_with_attribute(self, content, start, end):
+##        pattern_attribute = re.compile(self.regex_atribute);
+##        is_atttribute = lambda s : pattern_attribute.search(s) != None;
+        is_atttribute = lambda s : s.find('[') != -1 and s.find(']') != -1;
+        back_ind = start - 1;
+        while( True ):
+            if is_atttribute( content[back_ind] ):
+                back_ind -= 1;
+                continue;
+            break;
+        start = start if back_ind == start - 1 else back_ind + 1;
+        return start, end;
+
+# Analyze and remove duplicate classes in file
+class Analyze:
+    def __init__(self, file_tree):
+        self.common_filename = "CommonTypes.cs";
+        ##  [(filename, path)...]
+        self.file_tree = file_tree;
+        self.parser = Parser();
+        self.common_maps = None;
+        
+    def run(self):
+        print("Log: run analyze");
+        try:
+            path = ["{0}/{1}".format(pair[0], pair[1]) for pair in self.file_tree if pair[1] == self.common_filename];
+            path = str(path[0]);
+            # process common file
+            self.process_file(self.common_filename, path)
+            # process others
+            for pair in self.file_tree:
+                    path = "{0}/{1}".format(pair[0], pair[1]);
+                    self.process_file(pair[1], path);            
+        except Exception as e:
+            raise e;
+            print("Error : {0}".format(e.args[0]));
+            return;
+
+    def load_file(self, path):
+        fd = open(path, 'r');
+        cont = fd.readlines()
+        fd.close();
+        return cont;
+
+    def write_file(self, content, path):
+        print("Log: write file to : " + path);
+        fd = open(path, 'w');
+        [fd.write(str_) for str_ in content]
+        fd.close();
+
+    def process_file(self, filename, full_path):
+        if not os.path.exists(full_path):
+            raise Exception("{0} not found! Analyze stoped!".format(path));  
+        file_content = self.load_file(full_path);
+        if filename == self.common_filename:
+            if self.common_maps == None:
+                print("Log: process common file: " + self.common_filename);
+                # get map<fname, hash>
+                self.common_maps = self.parser.parse_file(file_content, True);
+        else:
+            print("Log: process file " + filename);
+            # get map<fname, (start, end)>
+            file_map = self.parser.parse_file(file_content, False);
+            clear_class = self.remove_enites(file_map, file_content);
+            # write file
+            self.write_file(clear_class, full_path);
+
+    # return <str> of *.cs file without class duplicate on file_map
+    def remove_enites(self, file_map, file_content):
+        print("Log: remove duplicate classes");
+        content = file_content[:]
+        for class_data, class_pos in file_map:
+            try:
+                # compare hashes
+                if self.common_maps[class_data[0]] == class_data[1]:
+                    # get class ranges with attributes
+                    start, end = self.parser.find_position_with_attribute(content, class_pos[0], class_pos[1])
+##                    print ("Old: [{} : {}]   New: [{} : {}]]".format(class_pos[0], class_pos[1], start , end));
+                    # remove class element
+                    for i in range(end, start - 1, -1):
+                        content.pop(i);
+                        content.insert(i, "\n");
+            except KeyError:
+                continue;
+        # remove new lines
+        ind_to_remove = [i for i, str_ in enumerate(content) if str_ == "\n"]
+        ind_to_remove.sort(reverse = True)
+        [content.pop(ind) for ind in ind_to_remove]
+        return content;
+    
 
 # Generator generate *.cs from *.xsd files
 class Generator:
@@ -90,13 +227,10 @@ class Generator:
             over_dial = input("Folder with three structure exists, overwrite? (y/n)? : default: 'n' ");
             if over_dial.lower() == 'y':
                 self.process_all();
-            else:
-                #TODO: Analyze files
-                self.analyzer.run();
         else:
             self.process_all();
-            #TODO: Analyze files
-            self.analyzer.run();
+        #TODO: Analyze files
+        self.analyzer.run();
 
     def process_all(self):
         # create directories
@@ -126,92 +260,6 @@ class Generator:
         except FileNotFoundError:
             print("Error: Specify {} as Path variable!".format(self.modes[self.mode]));
             exit(-1);
-
-# Analyze and remove duplicate classes in file
-class Analyze:
-    def __init__(self, file_tree):
-        self.common_filename = "CommonTypes.cs";
-        self.file_tree = file_tree;
-        self.common_filename_data = None;
-        self.parser = Parser();
-        
-    def run(self):
-        print("Log: run analyze");
-        try:
-            self.process_common_file();
-        except Exception as e:
-                print("Error : {0}".format(e.args[0]));
-                return;
-            
-    def process_common_file(self):
-        comm_file = self.parser.get_common_map;
-        if comm_file != None:
-            return;
-        print("Log: process common file");
-        path = ["{0}/{1}".format(pair[0], pair[1]) for pair in self.file_tree if pair[1] == self.common_filename];
-        path = str(path[0]);
-        if not os.path.exists(path):
-            raise Exception("{0} not found! Analyze stoped!".format(path));        
-        file_content = open(path, 'r').read();
-        print(file_content)
-        exit(-1)
-        
-        # parse file
-        comm_file = self.parser.parse_file(file_content, True);
-
-
-# C# simplify parser
-import re
-
-class Parser:    
-    def __init__(self):
-        self.regex_class_begin = r"class\s+([a-zA-Z]+)\s+{";
-        self.regex_atribute = r"\[[\w|\.|(|,|\"|)|=|\s]+]";
-        self.common_map_content = None;
-
-    @property
-    def get_common_map(self):
-        return self.common_map_content;
-
-    # Позволяет парсить классы из cs файлов
-    # для общего файла фозвращяет map<file, hash>
-    # для других map<file, (start_pos, end_pos)>
-    def parse_file(self, content, is_common_file):
-        print("Log: parse {} file".format("common" if is_common_file else "class"));
-        pattern_class_begin = re.compile(self.regex_class_begin);
-        pattern_attribute = re.compile(self.regex_atribute);
-        map_file = dict();
-        is_atttribute = lambda s : pattern_attribute.search(s) != None;
-        for i in range(len(content)):
-            match = pattern_class_begin.search(content[i])
-            if match != None:
-                start = i;
-                #find class end
-                open_braces = 1;
-                while(open_braces != 0):
-                    i += 1;
-                    st = content[i];
-                    if st.find('{') != -1:
-                        open_braces += 1;
-                    if st.find('}') != -1:
-                        open_braces -= 1;
-                end = i;
-                # find attributes
-                back_ind = start - 1;
-                while( True ):
-                    if is_atttribute( content[back_ind] ):
-                        back_ind -= 1;
-                        continue;
-                    break;
-                start = start if back_ind == start - 1 else back_ind + 1;
-                # add to map
-                if is_common_file:
-                    #print ("Class was found at {0}-{1}: name: {class_name}".format( start, end, class_name = match.groups()[0]))
-                    map_file[match.groups()[0]] = hash(str(content[start : end]));
-                else:
-                    map_file[match.groups()[0]] = (start, end);
-        print("Found: {} classes".format(len(map_file)));
-        return map_file;      
 
 
 # run app
